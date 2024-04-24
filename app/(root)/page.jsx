7e15f076d45a6ad6ec93d0c8e0f09e7a7def4137
Emma <mellgrenemma0@gmail.com>
2024-04-24 19:53:09 +0200

    first commit

---
+++ app/(root)/page.jsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center bg-slate-900">
+      <h1 className="mt-20 text-3xl font-semibold">My todos</h1>
+      
+    </div>
+  )
+}
+
+export default page
