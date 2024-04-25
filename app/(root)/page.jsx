57161acbc92b117fee977d97c46133bd586283c1
Emma <mellgrenemma0@gmail.com>
2024-04-25 19:05:11 +0200

    updated ui

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,15 +1,17 @@
 import Todos from '../../components/todos.jsx'
 import { Input } from '@/components/ui/input.jsx'
 import { ModeToggle } from '@/components/mode-toggle.jsx'
+import AddInput from '@/components/ui/add-input.jsx'
 
 const page = () => {  
   
   return (
-    <div className='w-screen min-h-screen bg-slate-900/70'>
-      <ModeToggle />
-      <main className="flex flex-col justify-center items-center  gap-24">
-        <div><Input /></div>
-        <div className=''><Todos /></div>
+    <div className='w-screen min-h-screen'>
+      <div className='pr-4 pt-4 flex justify-end'><ModeToggle /></div>
+      <main className="flex flex-col justify-center items-center gap-6">
+        <AddInput />
+        <div className='w-[400px]'><Input /></div>
+        <div><Todos /></div>
       </main>
     </div>
   )
