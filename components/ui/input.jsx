4d9bd54936908b71a91ab27077ba6c317168b718
Emma <mellgrenemma0@gmail.com>
2024-04-30 15:36:45 +0200

    updated ui

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium outline outline-2 outline-offset-2 outline-slate-700 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 placeholder:text-sm placeholder:text-muted-foreground/90 placeholder:font-medium",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium outline outline-2 outline-offset-2 outline-slate-700 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 placeholder:text-sm placeholder:text-muted-foreground/90 placeholder:font-medium",
           className
         )}
         ref={ref}
