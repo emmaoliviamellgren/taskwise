92721afddf4fb65c3ad8f87e318d0107e2477598
Emma <mellgrenemma0@gmail.com>
2024-04-30 14:09:10 +0200

    Add viewport meta tag to layout.jsx

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/app/(root)/(providers)/theme-provider';
 import TodoContextProvider from './(providers)/TodoContext';
+import Head from 'next/head';
 
 import '../globals.css';
 
@@ -16,7 +17,12 @@ export default function RootLayout({ children }) {
         <html
             lang='en'
             suppressHydrationWarning>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
+            <Head>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1.0, maximum-scale=1'
+                />
+            </Head>
             <body className={inter.className}>
                 <TodoContextProvider>
                     <ThemeProvider
